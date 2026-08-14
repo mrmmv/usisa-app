cffe1293d9547b2b057eddf9f3354021c2b632ef
mrmmv <meynard.villanueva@deped.gov.ph>
2026-08-14 13:19:40 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,17 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   base: './',
   server: {
     port: 3003,
     host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3004',
-        changeOrigin: true,
-      },
-    },
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: fileURLToPath(new URL('./index.html', import.meta.url)),
-        login: fileURLToPath(new URL('./login.html', import.meta.url))
-      }
-    }
   }
 });
+
 
 
 
